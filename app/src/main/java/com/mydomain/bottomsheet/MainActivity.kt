76eb501c4c757cf56c448a787a8df7ed8292e499
Yoann <yoann.chamillard@gmail.com>
2024-02-07 18:15:09 +0100

    163.PersistentBottomSheet

--- app/src/main/java/com/mydomain/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/mydomain/bottomsheet/MainActivity.kt
@@ -2,27 +2,57 @@ package com.mydomain.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import com.mydomain.bottomsheet.dialogs.MyBottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 
-class MainActivity : AppCompatActivity(), MyBottomSheetDialog.BottomSheetListener {
-    private lateinit var openDialogButton: Button
-    private lateinit var textViewButton: TextView
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private lateinit var textViewState: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        openDialogButton = findViewById(R.id.button_open_dialog)
-        textViewButton = findViewById(R.id.textview)
+        textViewState = findViewById(R.id.textview_state)
+        val view : View = findViewById(R.id.bottom_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(view)
 
-        openDialogButton.setOnClickListener {
-            val myBottomSheetDialog = MyBottomSheetDialog()
-            myBottomSheetDialog.show(supportFragmentManager, "Dialog")
+        val buttonOpen: Button = findViewById(R.id.button_open)
+        val buttonCollapse: Button = findViewById(R.id.button_collapse)
+
+        buttonOpen.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            textViewState.text = "EXPANDED"
         }
-    }
+        buttonCollapse.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            textViewState.text = "COLLAPSED"
+        }
 
-    override fun onButtonClicked(input: String) {
-        textViewButton.text = input
+        bottomSheetBehavior.addBottomSheetCallback(object:BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        textViewState.text = "EXPANDED"
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        textViewState.text = "DRAGGING"
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        textViewState.text = "SETTLING"
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        textViewState.text = "COLLAPSED"
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+        })
     }
 }
